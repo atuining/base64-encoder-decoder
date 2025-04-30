e0ce06bf4638d83bcef798303cb0257fd06bbf11
atuining <nagpalpranay@gmail.com>
2025-05-01 00:32:22 +0530

    hashmap for faster index lookup but had to use heap

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,11 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
-    var memory_buffer: [1000]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
-    const allocator = fba.allocator();
-
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
     const text = "Testing some more stuff";
     const etext = "VGVzdGluZyBzb21lIG1vcmUgc3R1ZmY=";
-    const base64 = Base64.init();
+    const base64 = try Base64.init(allocator);
     const encoded_text = try base64.encode(allocator, text);
     const decoded_text = try base64.decode(allocator, etext);
     try stdout.print("Encoded text: {s}\n", .{encoded_text});
@@ -17,13 +15,20 @@ pub fn main() !void {
 
 const Base64 = struct {
     _table: *const [64]u8,
+    _map: std.AutoHashMap(u8, usize),
 
-    pub fn init() Base64 {
+    pub fn init(allocator: std.mem.Allocator) !Base64 {
         const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         const lower = "abcdefghijklmnopqrstuvwxyz";
         const numbers_symb = "0123456789+/";
+        const table = upper ++ lower ++ numbers_symb;
+        var map = std.AutoHashMap(u8, usize).init(allocator);
+        for (0..table.len) |i| {
+            try map.put(table[i], i);
+        }
         return Base64{
-            ._table = upper ++ lower ++ numbers_symb,
+            ._table = table,
+            ._map = map,
         };
     }
 
@@ -31,16 +36,12 @@ const Base64 = struct {
         return self._table[index];
     }
 
-    fn _char_index(self: Base64, char: u8) u8 {
+    // TODO: use binary search
+    fn _char_index(self: Base64, char: u8) ?u8 {
         if (char == '=')
             return 64;
-        var index: u8 = 0;
-        for (0..63) |i| {
-            if (self._char_at(i) == char)
-                break;
-            index += 1;
-        }
-        return index;
+        const val: u8 = @intCast(self._map.get(char).?);
+        return val;
     }
 
     pub fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
@@ -104,7 +105,7 @@ const Base64 = struct {
         var buf = [4]u8{ 0, 0, 0, 0 };
 
         for (input, 0..) |_, i| {
-            buf[count] = self._char_index(input[i]);
+            buf[count] = self._char_index(input[i]).?;
             count += 1;
             if (count == 4) {
                 output[iout] = (buf[0] << 2) + (buf[1] >> 4);
